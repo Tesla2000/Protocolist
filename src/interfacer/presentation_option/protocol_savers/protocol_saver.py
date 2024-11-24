7673b1933bd9b4721933b1e769889459cdc37e29
Tesla2000 <fratajczak124@gmail.com>
2024-11-24 19:19:13 +0100

    Updated deduplication

--- src/interfacer/presentation_option/protocol_savers/protocol_saver.py
+++ src/interfacer/presentation_option/protocol_savers/protocol_saver.py
@@ -15,6 +15,7 @@ from ...protocol_markers.types_marker_factory import create_type_marker
 from ...transform.class_extractor import ClassExtractor
 from ..presentation_option import PresentationOption
 from ..replace_partial_with_combined import ReplaceImportsAndNames
+from ..replace_partial_with_combined import ReplaceNames
 
 
 class ProtocolSaver(ABC):
@@ -34,35 +35,50 @@ class ProtocolSaver(ABC):
         pass
 
     def _deduplicate(self):
-        extractor = ClassExtractor(create_type_marker(self.config))
         code = self.config.interfaces_path.read_text()
-        extracted_classes = extractor.extract_classes(code)
-        grouped_classes = map_reduce(
-            extracted_classes.items(),
-            lambda item: item[1]
-            .partition("(Protocol):\n")[-1]
-            .replace(": ...\n", ":\n        ...\n"),
-        )
-        unique_classes = dict(
-            sorted(
-                items,
-                key=lambda item: int(
-                    (re.findall(r"\d+", item[0]) or [sys.maxsize])[0]
-                ),
-            )[0]
-            for items in grouped_classes.values()
-        )
-        self.replace_dictionary = {
-            name: next(
-                filter(unique_classes.__contains__, map(itemgetter(0), names))
+        self.replace_dictionary = {}
+        replace_dictionary = None
+        while replace_dictionary != self.replace_dictionary:
+            replace_dictionary = self.replace_dictionary.copy()
+            extracted_classes = ClassExtractor(
+                create_type_marker(self.config)
+            ).extract_classes(code)
+            grouped_classes = map_reduce(
+                extracted_classes.items(),
+                lambda item: item[1]
+                .partition("(Protocol):\n")[-1]
+                .replace(": ...\n", ":\n        ...\n"),
             )
-            for names in grouped_classes.values()
-            for name, _ in names
-        }
-        imports = code.partition("@")[0]
-        self.config.interfaces_path.write_text(
-            imports + "".join(unique_classes.values())
-        )
+            unique_classes = dict(
+                sorted(
+                    items,
+                    key=lambda item: int(
+                        (re.findall(r"\d+", item[0]) or [sys.maxsize])[0]
+                    ),
+                )[0]
+                for items in grouped_classes.values()
+            )
+            self.replace_dictionary.update(
+                {
+                    name: next(
+                        filter(
+                            unique_classes.__contains__,
+                            map(itemgetter(0), names),
+                        )
+                    )
+                    for names in grouped_classes.values()
+                    for name, _ in names
+                }
+            )
+            self.replace_dictionary = {
+                key: self.replace_dictionary[value]
+                for key, value in self.replace_dictionary.items()
+            }
+            imports = code.partition("@")[0]
+            code = imports + "".join(unique_classes.values())
+            transformer = ReplaceNames(self.config, self.replace_dictionary)
+            code = libcst.parse_module(code).visit(transformer).code
+        self.config.interfaces_path.write_text(code)
 
     def _update_imports_and_names(self):
         for filepath in filter(
--- src/interfacer/presentation_option/replace_partial_with_combined.py
+++ src/interfacer/presentation_option/replace_partial_with_combined.py
@@ -10,33 +10,11 @@ from ..annotation2string import subscript_element2string
 from ..config import Config
 
 
-class ReplaceImportsAndNames(Transformer):
+class ReplaceNames(Transformer):
     def __init__(self, config: Config, replace_dictionary: dict[str, str]):
         super().__init__()
         self.config = config
         self.replace_dictionary = replace_dictionary
-
-    def leave_ImportFrom(
-        self, original_node: "ImportFrom", updated_node: "ImportFrom"
-    ) -> "ImportFrom":
-        if (
-            Module([updated_node]).code.split()[1]
-            == self.config.interface_import_path
-        ):
-            return updated_node.with_changes(
-                names=tuple(
-                    import_alias.with_changes(
-                        name=import_alias.name.with_changes(
-                            value=self.replace_dictionary.get(
-                                import_alias.name.value,
-                                import_alias.name.value,
-                            )
-                        )
-                    )
-                    for import_alias in updated_node.names
-                )
-            )
-        return updated_node
 
     def leave_Name(
         self, original_node: "Name", updated_node: "Name"
@@ -67,3 +45,27 @@ class ReplaceImportsAndNames(Transformer):
                 value=subscript_element2string(unique_slices[0]).strip('"')
             )
         )
+
+
+class ReplaceImportsAndNames(ReplaceNames):
+    def leave_ImportFrom(
+        self, original_node: "ImportFrom", updated_node: "ImportFrom"
+    ) -> "ImportFrom":
+        if (
+            Module([updated_node]).code.split()[1]
+            == self.config.interface_import_path
+        ):
+            return updated_node.with_changes(
+                names=tuple(
+                    import_alias.with_changes(
+                        name=import_alias.name.with_changes(
+                            value=self.replace_dictionary.get(
+                                import_alias.name.value,
+                                import_alias.name.value,
+                            )
+                        )
+                    )
+                    for import_alias in updated_node.names
+                )
+            )
+        return updated_node
